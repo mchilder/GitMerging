6222b75dee52d6622aa0b4e1092bca758ae3c18e
Ashley Marie Einspahr <aeinspah@bb136-22.mines.edu>
2014-02-20 16:03:23 -0700

    Typed Hi matt

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,6 +1,8 @@
 
 public class MyClass {
-	
+	public void aMethod(){
+		System.out.println("Hi Matt! How's it going?");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
