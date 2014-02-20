05f257fb7ec6e768af31c75d9ef909cb814f743f
Matthew Childers <mchilder@mymail.mines.edu>
2014-02-20 16:09:09 -0700

    Added anotherMehtod

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,11 +3,15 @@ public class MyClass {
 	public MyClass() {
 		// Constructor
 	}
+	public void anotherMethod() {
+		System.out.println("Sup Ashley!?!?");
+	}
 	public void aMethod(){
 		System.out.println("Hi Matt! How's it going?");
 	}
 	public static void main(String[] args) {
 		MyClass aClass = new MyClass();
 		aClass.aMethod();
+		aClass.anotherMethod();
 	}
 }
