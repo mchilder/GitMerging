e87c3db7adec39411c7593f4d3db4d2cb411f472
Ashley Einspahr <aeinspah@mines.edu>
2014-02-20 16:09:52 -0700

    Added something to aMethod

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	}
 	public void aMethod(){
 		System.out.println("Hi Matt! How's it going?");
+		System.out.println("I'm just going to keep typing things");
 	}
 	public static void main(String[] args) {
 		MyClass aClass = new MyClass();
